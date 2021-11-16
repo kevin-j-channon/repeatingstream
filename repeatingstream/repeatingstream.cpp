f3572b465ee7ba44b73265fa5b4aacbb779c58ec
kevin <kevinchannon@gmail.com>
2021-11-16 07:06:24 +0000

    Add project files.

---
+++ repeatingstream/repeatingstream.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace repeatingstream
+{
+	TEST_CLASS(repeatingstream)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
