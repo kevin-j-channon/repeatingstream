21a824f37ac96424bedbf3af0015a6aaef56969d
kevin <kevinchannon@gmail.com>
2021-11-17 07:53:21 +0000

    FIxing most cases

--- repeatingstream/repeating_stream.hpp
+++ repeatingstream/repeating_stream.hpp
@@ -1,47 +1,78 @@
 #pragma once
 
 #include <boost/iostreams/concepts.hpp>
+#include <boost/iostreams/stream.hpp>
+
 #include <utility>
 #include <algorithm>
 
-namespace repeatstream
+
+template<typename Buffer_T>
+class RepeatingInputSource : public boost::iostreams::source
 {
-    template<typename Buffer_T>
-    class SrcFromBuffer : public boost::iostreams::source {
-    public:
-        explicit source(Buffer_T&& buf, size_t repeat_count)
-            : m_buf{ std::move(buf) }
-            , m_current{ m_buf.cbegin() }
-            , m_remaining_repeats{ repeat_count }
-        {
-        }
+public:
+	RepeatingInputSource() = delete;
 
-        std::streamsize read(char_type* s, std::streamsize n)
-        {
-            const auto remainig = std::distance(m_current, m_buf.cend());
-            const auto read_count = std::streamsize{ std::min(n, remaining) };
-            const auto over_run = m_remaining_repeats > 0 ? n - read_count : 0;
+	explicit RepeatingInputSource(Buffer_T&& buf, size_t repeat_count)
+		: m_buf{ std::move(buf) }
+		, m_current{ m_buf.cbegin() }
+		, m_remaining_repeats{ repeat_count }
+	{
+	}
 
-            if (read_count > 0) {
-                it_current = std::copy_n(it_current, read_count, s);
-                if (over_run > 0) {
-                    --m_remaining_repeats;
-                    m_current = m)buf.cbegin();
+	RepeatingInputSource(const RepeatingInputSource& other)
+		: m_buf{ other.m_buf }
+		, m_current{ std::next(m_buf.cbegin(), std::distance(other.m_buf.cbegin(), other.m_current)) }
+		, m_remaining_repeats{ other.m_remaining_repeats }
+	{
+	}
 
-                    read_count += this->read(s + read_count, over_run);
-                }
+	RepeatingInputSource& operator=(const RepeatingInputSource& other)
+	{
+		m_buf = other.m_buf;
+		m_current = std::next(m_buf.cbegin(), std::distance(other.m_buf.cbegin(), other.m_current));
+		m_remaining_repeats = other.m_remaining_repeats;
+	}
 
-                return read_count;
-            }
-            else {
-                return -1; // EOF
-            }
-        }
+	RepeatingInputSource(RepeatingInputSource&&) noexcept = default;
+	RepeatingInputSource& operator=(RepeatingInputSource&&) noexcept = default;
 
-    private:
+	std::streamsize read(char_type* s, std::streamsize n)
+	{
+		const auto remaining = std::distance(m_current, m_buf.cend());
+		auto read_count = std::min<std::streamsize>(n, remaining);
+		const auto over_run = m_remaining_repeats > 1 ? n - read_count : 0;
 
-        Buffer_T m_buf;
-        Buffer_T::const_iterator m_current;
-        size_t m_remaining_repeats;
-    };
-}
+		if (read_count > 0) {
+			auto new_s = std::copy_n(m_current, read_count, s);
+			std::advance(m_current, read_count);
+
+			_handle_any_overrun_by_wrapping(over_run);
+
+			return read_count;
+		}
+		else {
+			return -1; // EOF
+		}
+	}
+
+private:
+	void _handle_any_overrun_by_wrapping(size_t over_run)
+	{
+		if (over_run <= 0) {
+			return;
+		}
+
+		--m_remaining_repeats;
+		m_current = m_buf.cbegin();
+
+		read_count += this->read(new_s, over_run);
+	}
+
+	Buffer_T m_buf;
+	typename Buffer_T::const_iterator m_current;
+	size_t m_remaining_repeats;
+};
+
+template<typename Buffer_T>
+using RepeatingInputStream = boost::iostreams::stream<RepeatingInputSource<Buffer_T>>;
--- repeatingstream/repeatingstream.cpp
+++ repeatingstream/repeatingstream.cpp
@@ -4,19 +4,67 @@
 
 #include <istream>
 #include <vector>
+#include <format>
+#include <iostream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace repeatingstream
+void fn(std::istream& is)
 {
-	TEST_CLASS(repeatingstream)
+
+	auto s = std::string();
+	std::getline(is, s);
+}
+
+namespace testrepeatingstream
+{
+	TEST_CLASS(SourceTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(RepeatCharacters5Times)
 		{
+			using Buffer_t = std::vector<char>;
+			RepeatingInputStream<Buffer_t> stream(RepeatingInputSource<Buffer_t>( Buffer_t{ 'a', 'b', 'c' }, 5));
 
-			std::istream test()
+			auto s = std::string();
+			stream >> s;
+			
+			Assert::AreEqual(std::string("abcabcabcabcabc"), s);
+		}
+
+		TEST_METHOD(RepeatCharacters1Times)
+		{
+			using Buffer_t = std::vector<char>;
+			RepeatingInputStream<Buffer_t> stream(RepeatingInputSource<Buffer_t>(Buffer_t{ 'a', 'b', 'c' }, 1));
+
+			auto s = std::string();
+			stream >> s;
+
+			Assert::AreEqual(std::string("abc"), s);
+		}
+
+		TEST_METHOD(RepeatCharacters0Times)
+		{
+			using Buffer_t = std::vector<char>;
+			RepeatingInputStream<Buffer_t> stream(RepeatingInputSource<Buffer_t>(Buffer_t{ 'a', 'b', 'c' }, 0));
+
+			auto s = std::string();
+			stream >> s;
+
+			Assert::AreEqual(std::string(""), s);
+		}
+
+		// This test is successful if it even builds.
+		TEST_METHOD(RepeatingInputStreamCanBePassedAsIstream)
+		{
+			using Buffer_t = std::vector<char>;
+			RepeatingInputStream<Buffer_t> stream(RepeatingInputSource<Buffer_t>(Buffer_t{ 'a', 'b', 'c' }, 5));
+
+			auto s = std::string();
+			stream >> s;
+
+			fn(stream);
 		}
 	};
 }
